4141b291ad28ef86e65e058b38a49cb60ea67353
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2022-07-11 12:48:32 -0500

    install dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,11 @@
-console.clear();
+const express = require('express');
+const dotenv = require('dotenv');
 
-console.log('Hello world');
-console.log('Hello world');
+dotenv.config();
+const app = express();
+
+app.get('/peliculas', (req, res) => {});
+
+app.listen(process.env.PORT, () => {
+	console.log('App corriendo en el puerto' + process.env.PORT);
+});
